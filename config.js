1fe4c78f53a6755b3a7ff5308246c858ea235a75
Suwarna-cloud <69640575+Suwarna-cloud@users.noreply.github.com>
2022-03-18 17:09:37 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+// import { initializeApp } from "firebase/app";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCBTfoaCaR-V5ztBawF3pBkaedPLIvtRXo",
+//   authDomain: "biomedic-5db57.firebaseapp.com",
+//   projectId: "biomedic-5db57",
+//   storageBucket: "biomedic-5db57.appspot.com",
+//   messagingSenderId: "293922219414",
+//   appId: "1:293922219414:web:6e00ac4df9d351028f29b7"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export default app;
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqCpWg9CNcRm9Us8sWYRAj1Z9DXED-EB4",
+  authDomain: "biomedic-e051e.firebaseapp.com",
+  projectId: "biomedic-e051e",
+  storageBucket: "biomedic-e051e.appspot.com",
+  messagingSenderId: "1026643665552",
+  appId: "1:1026643665552:web:67874031b60149e42bee15",
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
